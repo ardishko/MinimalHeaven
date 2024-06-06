5c15b7e5beb3db9cfac556137ad686721deb7c24
ardishco <ardishco@protonmail.com>
2024-06-06 23:03:18 +0300

    removed fonts

--- modules/core/packages/packages.nix
+++ modules/core/packages/packages.nix
@@ -1,25 +1,10 @@
 {
   pkgs,
-  inputs,
-  lib,
   options,
   ...
 }: {
   environment.systemPackages = with pkgs; [
     bash
     neovim
-  ];
-
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-    nerdfonts
   ];
 }
