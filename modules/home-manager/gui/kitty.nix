d66e4d4ca3bc7f7e49201ee6a907543e036f9d79
ardishco <ardishco@protonmail.com>
2024-06-06 23:03:57 +0300

    removed reference to font in kitty

--- modules/home-manager/gui/kitty.nix
+++ modules/home-manager/gui/kitty.nix
@@ -2,10 +2,6 @@
   programs.kitty = {
     enable = true;
     theme = "Catppuccin-Frappe";
-    font = {
-      name = "Iosevka Nerd Font";
-      size = 13;
-    };
     settings = {
       confirm_os_window_close = 0;
     };
