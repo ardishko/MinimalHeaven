c5c42210ac417ff13ea5243ae4fa15cf13f1b72d
ardishco <ardishco@protonmail.com>
2024-06-25 01:08:27 +0300

    username thingy

--- machines/BigShell/configuration.nix
+++ machines/BigShell/configuration.nix
@@ -34,12 +34,13 @@
   services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.liquid = {
+  users.users.${username} = {
     isNormalUser = true;
-    description = "liquid";
+    description = "${username}";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
     ];
+    initialPassword = "nutsitch";
     shell = pkgs.nushell;
   };
 
